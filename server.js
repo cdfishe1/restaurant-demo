bffef2edabf755ba14c06ccb0b48aede6dcbb798
cdfishe1 <75449671+cdfishe1@users.noreply.github.com>
2021-04-12 19:26:57 -0500

    Add server.js file

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const path = require('path');
+
+// Sets up the Express App
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Basic route that sends the user first to the AJAX Page
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'home.html')));
+
+app.get('/reserve', (req, res) => res.sendFile(path.join(__dirname, 'reserve.html')));
+
+app.get('/tables', (req, res) => res.sendFile(path.join(__dirname, 'tables.html')));
+
+
+
+require('./routes/apiRoutes')(app);
+require('./routes/htmlRoutes')(app);
+
+app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
